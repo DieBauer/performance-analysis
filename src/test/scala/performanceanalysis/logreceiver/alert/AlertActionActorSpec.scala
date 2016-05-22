447b9938cbd7a162919ad65b9a0cf58d7c9d91a6
Seeta <srvadali@gmail.com>
2016-05-22 16:51:07 +0200

    [Stuck] trying to improve unit test

--- src/test/scala/performanceanalysis/logreceiver/alert/AlertActionActorSpec.scala
+++ src/test/scala/performanceanalysis/logreceiver/alert/AlertActionActorSpec.scala
@@ -1,23 +1,59 @@
 package performanceanalysis.logreceiver.alert
 
 import akka.actor.{ActorSystem, Props}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import akka.testkit.TestProbe
 import performanceanalysis.base.ActorSpecBase
 import performanceanalysis.server.Protocol._
+import performanceanalysis.server.Server
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util.{Failure, Success}
 
 class AlertActionActorSpec(testSystem: ActorSystem) extends ActorSpecBase(testSystem) {
+  val message: String = "log message"
   def this() = this(ActorSystem("AlertActionActorSpec"))
 
   "AlertActionActor" must {
+
     val alertActionActor = system.actorOf(Props[AlertActionActor])
 
     "send out an alert to a given endpoint when it receives an AlertingRuleViolated message " in {
       val testProbe = TestProbe("testProbe")
+      import system.dispatcher
+      val mockHttp = new MockHttp
+      val host = Await.result(mockHttp.getServerAddress, 1 second).getHostString
 
-      testProbe.send(alertActionActor, AlertRuleViolated("http://somewhere.net", "One of your loglines violated an alert rule"))
+      val url = s"http://$host/valid"
+      testProbe.send(alertActionActor, AlertRuleViolated(url, message))
       // Here I would like to verify that the alertActionActor sent out a request. Is there a possibility to do so?
       testProbe.expectNoMsg()
+
     }
   }
+}
 
+class MockHttp extends Server {
+  override protected def httpInterface: String = "localhost"
+
+  override protected def httpPort: Int = 8082
+
+  override protected def componentsRoute: Route = pathPrefix("valid") {
+    post {
+      entity(as[String]) { body =>
+        if (body.isEmpty) {
+          failWith(new IllegalArgumentException("message is not posted"))
+        } else {
+          complete("good request")
+        }
+      }
+    }
+  } ~ pathPrefix("invalid") {
+    put {
+      failWith(new RuntimeException("why did you post"))
+    }
+  }
 }
